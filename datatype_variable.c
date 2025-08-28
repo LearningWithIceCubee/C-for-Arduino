7adf6aa03cc2aca75e12538d871dd7c82586568d
IceCubee <waiyanhein8140@gmail.com>
2025-08-28 11:30:12 +0630

    Create datatype_variable.c

---
+++ datatype_variable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int number = 13;
+char alphabet = 'a';
+float pi = 3.142;
+
+int main(){
+    printf("This is the number = %d", number);
+    printf("\nThis is the alphabet = %c", alphabet);
+    printf("\nThis is the PI = %f", pi);
+    printf("\nThis\tThat");
+    return 0;
+    
+    return 0;
+}
